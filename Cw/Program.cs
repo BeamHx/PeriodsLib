fc777aeb8edc3ced4ffa9ad3e17cb47f0e972a1e
dogequake <hypek60@gmail.com>
2023-10-23 14:58:17 +0500

    uysdf

--- Cw/Program.cs
+++ Cw/Program.cs
@@ -13,10 +13,11 @@ namespace Cw
         static void Main(string[] args)
         {
             //
+            //
             TimeSpan begin = new TimeSpan(8, 0, 0);
             TimeSpan end = new TimeSpan(20, 0, 0);
             TimeSpan dur = new TimeSpan(0, 30, 0);
-            int[] duration = { 60, 30, 10, 10, 40,10 };
+            int[] duration = { 60, 30, 10, 10, 40,30 };
             TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0), new TimeSpan(15, 0, 0), new TimeSpan(15, 30, 0), new TimeSpan(16, 50, 0), new TimeSpan(18, 0, 0), };
             int j = 0;
             for (var i = begin; i < end; i+=dur)
