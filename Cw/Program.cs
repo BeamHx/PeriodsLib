a7fac032ca2505c9fe96eea009b99dec01ddd417
dogequake <hypek60@gmail.com>
2023-10-23 14:56:40 +0500

    sdffsd

--- Cw/Program.cs
+++ Cw/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,39 @@ namespace Cw
         static void Main(string[] args)
         {
             TimeSpan begin = new TimeSpan(8, 0, 0);
-            TimeSpan end = new TimeSpan(18, 0, 0);
+            TimeSpan end = new TimeSpan(20, 0, 0);
             TimeSpan dur = new TimeSpan(0, 30, 0);
-            int[] duration = { 60, 30, 10, 10, 40 };
-            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0), new TimeSpan(15, 0, 0), new TimeSpan(15, 30, 0), new TimeSpan(16, 50, 0), };
-
+            int[] duration = { 60, 30, 10, 10, 40,10 };
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0), new TimeSpan(15, 0, 0), new TimeSpan(15, 30, 0), new TimeSpan(16, 50, 0), new TimeSpan(18, 0, 0), };
+            int j = 0;
             for (var i = begin; i < end; i+=dur)
             {
-               
-                for (int j = 0; j < duration.Length; j++)
+
+                if ( j <= startTimes.Length - 1 && startTimes[j] == i)
                 {
-                    if (startTimes[j] == i || i + new TimeSpan(0, dur[j], 0) < startTimes[j] - )
+                    i += new TimeSpan(0, duration[j], 0);
+
+                    j++;
+
+                    if (j <= startTimes.Length - 1 && startTimes[j] == i)
                     {
-                        i += new TimeSpan(0,duration[j],0);
-                        break;
+                        i += new TimeSpan(0, duration[j], 0);
+                        j++;
                     }
+                    if (j <= startTimes.Length - 1 && startTimes[j] - i < dur)
+                    {
+                        i = startTimes[j] + new TimeSpan(0, duration[j], 0);
+                        j++;
+                    }
+
                 }
+
+                if(i < startTimes[startTimes.Length- 1 ] && j <= startTimes.Length - 1 && startTimes[j] - i < dur)
+                {
+                    i = startTimes[j] + new TimeSpan(0, duration[j], 0);
+                    j++;
+                }
+                
                 Console.WriteLine(i);
                 
             }
--- PeriodsLib/Calculations.cs
+++ PeriodsLib/Calculations.cs
@@ -8,18 +8,44 @@ namespace PeriodsLib
 {
     public class Calculations
     {
-        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime , TimeSpan endWorkingTime, int consultationTime)
-        {
-            string[] free = { "" };
+        //public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime , TimeSpan endWorkingTime, int consultationTime)
+        //{
+        //    string[] free = { "" };
 
-            for (var  i = beginWorkingTime;  i < endWorkingTime;)
-            {
-               
+        //    int j = 0;
+        //    int k = 0;
+        //    for (var i = beginWorkingTime; i < endWorkingTime; i += new TimeSpan(0, 30, 0);)
+        //    {
 
-            }
+        //        if (startTimes[j] == i)
+        //        {
+        //            i += new TimeSpan(0, duration[j], 0);
 
-            return free;
-            
-        }
+        //            j++;
+
+        //            if (startTimes[j] == i)
+        //            {
+        //                i += new TimeSpan(0, duration[j], 0);
+        //                j++;
+        //            }
+        //            if (startTimes[j] - i < dur)
+        //            {
+        //                i = startTimes[j] + new TimeSpan(0, duration[j], 0);
+        //                j++;
+        //            }
+
+        //        }
+
+        //        if (startTimes[j] - i < dur)
+        //        {
+        //            i = startTimes[j] + new TimeSpan(0, duration[j], 0);
+        //        }
+
+        //        free[k] = i;
+        //        k++;
+
+        //}
+        //    return free;
+        //}
     }
 }
